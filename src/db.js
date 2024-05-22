df9d82844f22a4015b89e6c7057897219e004268
Kevin Ordoñez <kevin.2000villegas@gmail.com>
2024-05-21 22:50:55 -0500

    feat: Update database connection configuration and remove commented code

--- src/db.js
+++ src/db.js
@@ -8,4 +8,23 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME
 });
 
+// pool.getConnection((err, connection) => {
+//     if (err) {
+//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//             console.error('Conexión a la base de datos cerrada');
+//         }
+//         if (err.code === 'ER_CON_COUNT_ERROR') {
+//             console.error('La base de datos tiene muchas conexiones');
+//         }
+//         if (err.code === 'ECONNREFUSED') {
+//             console.error('Conexión a la base de datos rechazada');
+//         }
+//     }
+//     if (connection) {
+//         connection.release();
+//         console.log('Conexión a la base de datos exitosa');
+//     }
+//     return;
+// });
+
 module.exports.pool = pool;
