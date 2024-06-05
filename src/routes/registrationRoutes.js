196cae39cf3abad35128e65a268075eca4afee49
Kevin Ordoñez <kevin.2000villegas@gmail.com>
2024-06-05 00:59:12 -0500

    add: getRegistrationsBy user funtion

--- src/routes/registrationRoutes.js
+++ src/routes/registrationRoutes.js
@@ -4,6 +4,7 @@ const {
   getRegistrationById,
   createRegistration,
   updateRegistration,
+  deleteRegistrationByUser,
   deleteRegistration
 } = require("../controllers/registrationController");
 
@@ -13,6 +14,7 @@ router.get("/get-all", getAllRegistrations);                 // Obtener todas la
 router.get("/:id", getRegistrationById);              // Obtener una inscripción por ID
 router.post("/create", createRegistration);                 // Crear una nueva inscripción
 router.put("/update/:id", updateRegistration);               // Actualizar una inscripción existente
+router.delete("/delete-by-user", deleteRegistrationByUser); // Eliminar inscripciones de un usuario
 router.delete("/delete/:id", deleteRegistration);            // Eliminar una inscripción
 
 module.exports = router;
